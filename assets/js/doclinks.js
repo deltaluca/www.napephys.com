7aaeeb52084584dbd64e48966b3255802a529eca
Luca Deltodesco <luca@deltaluca.me.uk>
2012-10-31 10:43:14 +0000

    Add callbacks section, lacking PreListener.

--- assets/js/doclinks.js
+++ assets/js/doclinks.js
@@ -8,6 +8,8 @@ function applyDocLinks()
     links["Constraint"] = links["Constraints"] = "types/nape/constraint/Constraint.html";
     links["Compound"] = links["Compounds"] = "types/nape/phys/Compound.html";
     links["CbType"] = links["CbTypes"] = "types/nape/callbacks/CbType.html";
+    links["CbEvent"] = links["CbEvents"] = "types/nape/callbacks/CbEvent.html";
+    links["OptionType"] = links["OptionTypes"] = "types/nape/callbacks/OptionType.html";
     links["Material"] = links["Materials"] = "types/nape/phys/Material.html";
     links["Circle"] = links["Circles"] = "types/nape/shape/Circle.html";
     links["Polygon"] = links["Polygons"] = "types/nape/shape/Polygon.html";
@@ -25,6 +27,9 @@ function applyDocLinks()
     links["nape-symbolic"] = "index.html#mod-nape-symbolic";
     links["SymbolicConstraint"] = "types/nape/symbolic/SymbolicConstraint.html";
     links["Listener"] = links["Listeners"] = "types/nape/callbacks/Listener.html";
+    links["BodyListener"] = links["BodyListeners"] = "types/nape/callbacks/BodyListener.html";
+    links["InteractionListener"] = links["InteractionListeners"] = "types/nape/callbacks/InteractionListener.html";
+    links["ConstraintListener"] = links["ConstraintListeners"] = "types/nape/callbacks/ConstraintListener.html";
     links["PreListener"] = links["PreListeners"] = "types/nape/callbacks/PreListener.html";
     links["Geom"] = "types/nape/geom/Geom.html";
     links["BitmapDebug"] = "types/nape/util/BitmapDebug.html";
@@ -34,12 +39,19 @@ function applyDocLinks()
     links["Debug"] = "types/nape/util/Debug.html";
     links["Mat23"] = "types/nape/geom/Mat23.html";
     links["Callback"] = "types/nape/callbacks/Callback.html";
-    links["InteractionListener"] = "types/nape/callbacks/InteractionListener.html";
     links["Arbiter"] = links["Arbiters"] = "types/nape/dynamics/Arbiter.html";
     links["Contact"] = links["Contacts"] = "types/nape/dynamics/Contact.html";
     links["FluidProperties"] = "types/nape/phys/FluidProperties.html";
     links["InteractionFilter"] = links["InteractionFilters"] = "types/nape/dynamics/InteractionFilter.html";
     links["InteractionGroup"] = links["InteractionGroups"] = "types/nape/dynamics/InteractionGroup.html";
+    links["Vec2List"] = links["Vec2Lists"] = "types/nape/geom/Vec2List.html";
+    links["CbTypeList"] = links["CbTypeLists"] = "types/nape/callbacks/CbTypeList.html";
+    links["BodyCallback"] = "types/nape/callbacks/BodyCallback.html";
+    links["InteractionCallback"] = "types/nape/callbacks/InteractionCallback.html";
+    links["InteractionType"] = "types/nape/callbacks/InteractionType.html";
+    links["ConstraintCallback"] = "types/nape/callbacks/ConstraintCallback.html";
+    links["PreCallback"] = "types/nape/callbacks/PreCallback.html";
+    links["Interactor"] = links["Interactors"] = "types/nape/phys/Interactor.html";
 
     function docLink(b)
     {
